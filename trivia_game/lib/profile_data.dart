d4faabad3e8b0440a008f8f2d82b29233b641f6f
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-02-25 23:43:28 +0200

    name get & set methods

--- trivia_game/lib/profile_data.dart
+++ trivia_game/lib/profile_data.dart
@@ -1,3 +1,5 @@
+import 'package:trivia_game/api/preferences.dart';
+
 class ProfileData {
   String name = "";
 
@@ -5,11 +7,18 @@ class ProfileData {
     this.name = "",
   });
 
-  void setName(String name) {
-    this.name = name;
+  void setName(String inputname) {
+    name = inputname;
+    Preferences().setName(name);
+  }
+
+  void setNameFromPreferences(String prefname) {
+    print("Setting name from preferences: $prefname");
+    name = prefname;
   }
 
   String getName() {
+    print("Getting name: $name");
     return name;
   }
 }
