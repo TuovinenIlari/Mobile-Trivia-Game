95219a135a0eb74f61ea4013c1277809bccf65f4
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-02-25 22:17:49 +0200

    Profile data class

---
+++ trivia_game/lib/profile_data.dart
@@ -0,0 +1,15 @@
+class ProfileData {
+  String name = "";
+
+  ProfileData({
+    this.name = "",
+  });
+
+  void setName(String name) {
+    this.name = name;
+  }
+
+  String getName() {
+    return name;
+  }
+}
