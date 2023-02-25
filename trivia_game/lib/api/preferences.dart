c4ce2f12fed6c1a4f54bb974391b54f6e952380d
Ilari Tuovinen <ilari.tuovinen@hotmail.com>
2023-02-25 23:38:49 +0200

    working crud for pref name

--- trivia_game/lib/api/preferences.dart
+++ trivia_game/lib/api/preferences.dart
@@ -3,17 +3,22 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Preferences {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
-  String getName() {
+  Future<String> getName() async {
     String name = '';
-    _prefs.then((SharedPreferences prefs) {
-      name = prefs.getString('name') ?? '';
-    });
+    final SharedPreferences prefs = await _prefs;
+    name = prefs.getString('name')!;
+    print("Getting name from preferences: $name");
+
     return name;
   }
 
-  void setName(String name) {
-    _prefs.then((SharedPreferences prefs) {
-      prefs.setString('name', name);
-    });
+  void setName(String name) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setString('name', name);
+  }
+
+  void clearName() async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.remove('name');
   }
 }
